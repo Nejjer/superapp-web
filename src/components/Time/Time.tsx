c545e2aef07cce6b9dcd9fdb2bfe20b40564e110
devofy <artemijv65@gmail.com>
2024-08-19 23:03:14 +0500

    Сделал ВРЕМЯ обновляемым

--- src/components/Time/Time.tsx
+++ src/components/Time/Time.tsx
@@ -2,15 +2,29 @@ import { Card } from '../Card';
 import styles from './style.module.scss';
 import moment from 'moment';
 import 'moment/dist/locale/ru';
+import { useEffect, useRef, useState } from 'react';
+
+const getDate = () => moment().locale('ru').format('ddd, D MMM');
+const getTime = () => moment().format('HH:mm');
 
 export const Time = () => {
+  const [date, setDate] = useState(() => getDate());
+  const [time, setTime] = useState(() => getTime());
+  const intervalId = useRef(0);
+
+  useEffect(() => {
+    intervalId.current = setInterval(() => {
+      setTime(getTime());
+      setDate(getDate());
+    }, 1000);
+    return () => clearInterval(intervalId.current);
+  }, []);
+
   return (
     <Card radius={'9999px'} className={styles.card}>
       <div className={styles.container}>
-        <div className={styles.date}>
-          {moment().locale('ru').format('ddd, D MMM')}
-        </div>
-        <div className={styles.time}>{moment().format('HH:mm')}</div>
+        <div className={styles.date}>{date}</div>
+        <div className={styles.time}>{time}</div>
       </div>
     </Card>
   );
