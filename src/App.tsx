72401cb99ba0d5dcbad9d8b5f8a1a7552ecf38ee
Devofy <artemijv65@gmail.com>
2025-05-13 00:11:59 +0500

    Добавил react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,16 @@
-import { Time } from './components/Time';
-import { useEffect, useRef, useState } from 'react';
-import { api, IPCInfo } from './api/api.ts';
-import { PCInfo } from './components/PCInfo';
-import styles from './index.module.scss';
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Compumon } from "./views/Compumon";
+import { RetroWeek } from "./views/RetroWeek";
 
 function App() {
-  const [pcInfo, setPCInfo] = useState<IPCInfo>();
-  const [serverInfo, setServerInfo] = useState<IPCInfo>();
-  const timeoutId = useRef<number>();
-
-  useEffect(() => {
-    handleSetTimeout();
-    return () => {
-      clearTimeout(timeoutId.current);
-      console.log('CLEAR TIMEOUT');
-    };
-  }, []);
-
-  const handleSetTimeout = () => {
-    timeoutId.current = window.setTimeout(() => {
-      fetchInfo().then(() => {
-        handleSetTimeout();
-      });
-    }, 2000);
-  };
-
-  const fetchInfo = async () => {
-    try {
-      api.getPCInfo().then((res) => {
-        setPCInfo(res);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-    try {
-      api.getServerInfo().then((res) => {
-        setServerInfo(res);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
-    <div className={styles.AppContainer}>
-      <div className={styles.App}>
-        <Time />
-        <PCInfo pcInfo={pcInfo}deviveName="PC" />
-        <PCInfo
-          pcInfo={serverInfo}
-          deviveName="Server"
-        />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Compumon />} />
+        <Route path="/" element={<RetroWeek />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/Compumon.tsx
@@ -0,0 +1,59 @@
+import { FC, useEffect, useRef, useState } from 'react';
+import styles from './index.module.scss';
+import { api, IPCInfo } from '../api/api.ts';
+import { Time } from '../components/Time';
+import { PCInfo } from '../components/PCInfo';
+
+export const Compumon: FC = () => {
+    const [pcInfo, setPCInfo] = useState<IPCInfo>();
+    const [serverInfo, setServerInfo] = useState<IPCInfo>();
+    const timeoutId = useRef<number>();
+
+    useEffect(() => {
+        handleSetTimeout();
+        return () => {
+            clearTimeout(timeoutId.current);
+            console.log('CLEAR TIMEOUT');
+        };
+    }, []);
+
+    const handleSetTimeout = () => {
+        timeoutId.current = window.setTimeout(() => {
+            fetchInfo().then(() => {
+                handleSetTimeout();
+            });
+        }, 2000);
+    };
+
+    const fetchInfo = async () => {
+        try {
+            api.getPCInfo().then((res) => {
+                setPCInfo(res);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+        try {
+            api.getServerInfo().then((res) => {
+                setServerInfo(res);
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className={styles.AppContainer}>
+            <div className={styles.App}>
+                <Time />
+                <PCInfo pcInfo={pcInfo} deviveName="PC" />
+                <PCInfo
+                    pcInfo={serverInfo}
+                    deviveName="Server"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default App;
