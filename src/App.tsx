bfc02ee4d008309e90976cea7490dfc114e7d342
Devofy <artemijv65@gmail.com>
2025-05-18 01:17:55 +0500

    Сделал PWA

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { Compumon } from "./views/Compumon";
 import { RetroWeek } from "./views/RetroWeek/RetroWeek";
 import { Provider } from "./components/ui/provider";
 import { Toaster } from "./components/ui/toaster";
+import { Theme } from "@chakra-ui/react";
 
 function App() {
 
   return (
     <Provider>
-      <BrowserRouter basename="/app">
-        <Routes>
-          <Route path="/" element={<Compumon />} />
-          <Route path="/retro" element={<RetroWeek />} />
-        </Routes>
-        <Toaster />
-      </BrowserRouter>
+      <Theme appearance="dark">
+        <BrowserRouter basename="/app">
+          <Routes>
+            <Route path="/" element={<Compumon />} />
+            <Route path="/retro" element={<RetroWeek />} />
+          </Routes>
+          <Toaster />
+        </BrowserRouter>
+      </Theme>
     </Provider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Super-App',
+      short_name: 'SuperApp',
+      description: 'Все в одном',
+      theme_color: '#282c34',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  })],
   server: {
     host: '0.0.0.0',
     port: 3000,
